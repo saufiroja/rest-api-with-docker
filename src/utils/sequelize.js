3714c0228626b455a045f98de8327d8e555e7b7b
saufiroja <saufiroja08@gmail.com>
2022-03-10 19:43:22 +0800

    delete: pool

--- src/utils/sequelize.js
+++ src/utils/sequelize.js
@@ -1,27 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-const {
-  DB_HOST,
-  DB_USER,
-  DB_PASS,
-  DB_NAME,
-  DB_SSL,
-  POOL_MAX,
-  POOL_MIN,
-  POOL_ACQUIRE,
-  POOL_IDLE,
-} = process.env;
+const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_SSL } = process.env;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
   host: DB_HOST,
   dialect: 'postgres',
   logging: false,
-  pool: {
-    max: POOL_MAX,
-    min: POOL_MIN,
-    acquire: POOL_ACQUIRE,
-    idle: POOL_IDLE,
-  },
   dialectOptions: {
     ssl: DB_SSL === 'true',
   },
