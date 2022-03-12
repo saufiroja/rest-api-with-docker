fb855f3f624fc8855d8cabeb554961e57fd66c36
saufiroja <saufiroja08@gmail.com>
2022-03-12 18:33:59 +0800

    feat: delete, get, and update

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -1,8 +1,9 @@
+const { Op } = require('sequelize');
 const { Post } = require('../database/models');
 
-// Desc     : POST Content
-// Routes   : POST api/post
-// access   : private
+// @Desc     : POST Content
+// @Routes   : POST api/post
+// @access   : private
 const createPost = async (req, res, next) => {
   try {
     const { title, content } = req.body;
@@ -27,16 +28,16 @@ const createPost = async (req, res, next) => {
   }
 };
 
-// Desc     : GET Content
-// Routes   : GET api/posts
-// access   : Public
+// @Desc     : GET Content
+// @Routes   : GET api/posts
+// @access   : Public
 const getAllPosts = async (req, res, next) => {
   try {
     const posts = await Post.findAll({
       attributes: { exclude: ['deletedAt'] },
     });
 
-    if (posts < 0) {
+    if (posts.length == 0) {
       return res.status(404).json({
         message: 'posts not found',
       });
@@ -51,4 +52,132 @@ const getAllPosts = async (req, res, next) => {
   }
 };
 
-module.exports = { createPost, getAllPosts };
+// @Desc    : Get posts from user
+// @Routes  : GET api/user/posts
+// @access  : Private
+const getAllPostsByUser = async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({ where: { userId: req.user.id } });
+
+    if (posts.length == 0) {
+      return res.status(404).json({
+        message: 'post not found',
+      });
+    }
+
+    return res.status(200).json({
+      message: 'successfully get all posts user',
+      posts,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @Desc    : Get search post
+// @Routes  : GET api/post
+// @access  : public
+const getSearchPost = async (req, res, next) => {
+  try {
+    const { title } = req.query;
+    const post = await Post.findAll({
+      where: {
+        title: {
+          [Op.substring]: `%${title}%`,
+        },
+      },
+    });
+
+    if (post.length == 0) {
+      return res.status(404).json({
+        message: 'post not found',
+      });
+    }
+
+    return res.status(200).json({
+      message: 'successfully search post',
+      post,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @Desc    : Update Post
+// @Routes  : PUT api/post/:id
+// @access  : Private
+const updatePost = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    const post = await Post.findByPk(id);
+    if (!post) {
+      return res.status(404).json({
+        message: 'post not found',
+      });
+    }
+
+    if (!req.user) {
+      return res.status(404).json({
+        message: 'user not found',
+      });
+    }
+
+    if (post.userId !== req.user.id) {
+      return res.status(401).json({
+        message: 'unauthorized',
+      });
+    }
+
+    await Post.update({ title, content }, { where: { id } });
+    return res.status(201).json({
+      message: 'successfully update post',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// @Desc    : Delete Post
+// @Routes  : DELETE api/post/:id
+// @access  : Private
+const deletePost = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findByPk(id);
+    if (!post) {
+      return res.status(404).json({
+        message: 'post not found',
+      });
+    }
+
+    if (!req.user) {
+      return res.status(404).json({
+        message: 'user not found',
+      });
+    }
+
+    if (post.userId !== req.user.id) {
+      return res.status(401).json({
+        message: 'unauthorized',
+      });
+    }
+
+    await Post.destroy({ where: { id } });
+    return res.status(200).json({
+      message: 'successfully delete post',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createPost,
+  getAllPosts,
+  getAllPostsByUser,
+  getSearchPost,
+  updatePost,
+  deletePost,
+};
--- src/routers/post.routers.js
+++ src/routers/post.routers.js
@@ -1,9 +1,23 @@
 const router = require('express').Router();
 
-const { createPost, getAllPosts } = require('../controllers/post.controllers');
+const {
+  createPost,
+  getAllPosts,
+  getAllPostsByUser,
+  getSearchPost,
+  updatePost,
+  deletePost,
+} = require('../controllers/post.controllers');
 const { isAuthentication } = require('../middlewares/auth.middlewares');
 
-router.get('/posts', isAuthentication, getAllPosts);
+router.get('/posts', getAllPosts);
+router.get('/post', getSearchPost);
+router.get('/user/posts', isAuthentication, getAllPostsByUser);
+
 router.post('/post', isAuthentication, createPost);
+
+router.put('/post/:id', isAuthentication, updatePost);
+
+router.delete('/post/:id', isAuthentication, deletePost);
 
 module.exports = router;
